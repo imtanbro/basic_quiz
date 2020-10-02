b1ec81acf9b51e1d76082aaf94080fd3d6dc9966
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-02 20:16:35 +0530

    Improving UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,20 @@ void main() {
   runApp(MyApp());
 }
 
+
+//trying to write a function
+
+// stylingSub(String name){
+//     Text("$name",
+//     style: TextStyle(
+//         color: Colors.white,
+//         fontSize: 20
+//     ),);
+// }
+
+
+
+
 class MyApp extends StatelessWidget {
   //important anotaion... it is so called decorator
 
@@ -27,7 +41,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
 
-        backgroundColor: Colors.black87,
+        backgroundColor: Colors.black12,
         // body: Center(
         //   child: Text("My default Text",
         //   style: TextStyle(
@@ -35,17 +49,56 @@ class MyApp extends StatelessWidget {
         //     fontSize: 25),),
         // ),
 
-        body: Column(
-          children: [
-            //[] dinots takes in list.
-              Text("The Questions!",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20),
-                )
+        body: Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Center(
+            child: Column(
+              children: [
+                //[] dinots takes in list.
+                  
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 50),
+                    child: Text("The Questions!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20
+                      ),
+                    ),
+                  ),
+
+                  RaisedButton(child: Text("Answer 1",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20, 
+                  ),),
+                  onPressed: null,),
+
+                  RaisedButton(child: Text("Answer 2",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20, 
+                  ),),
+                  onPressed: null,),
+
+                  RaisedButton(child: Text("Answer 3",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20, 
+                  ),),
+                  onPressed: null,
+                  ),
+
+                  RaisedButton(child: Text("Answer 4",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20, 
+                  ),),
+                  onPressed: null,),
 
 
-          ],
+              ],
+            ),
+          ),
         ),
       ),
     );
