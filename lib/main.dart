af00a8a5954258d67461a10e36dc0666807e4d4a
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-02 18:04:11 +0530

    Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
 //extends informs dart that this calss willl be based on some other class
 //and we can extext only 1 class at a time
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Text("Hey"),
+    );
   } //build is a method
 
 }
