3b0927cd32bbb5eee7e6e75f53ac3d32844f046f
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-02 19:35:39 +0530

    Adding a scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,31 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  //important anotaion... it is so called decorator
+  
+  @override
 //extends informs dart that this calss willl be based on some other class
 //and we can extext only 1 class at a time
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hey"),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(255, 255, 255, 0.1),
+          title: 
+          Text("My First App",
+          style: TextStyle(
+            color: Colors.yellowAccent),
+          ),),
+          backgroundColor: Colors.black87,
+          body: Center(
+            child: Text("My default Text",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25),),
+          ),
+
+      ),
+      
     );
   } //build is a method
-
 }
