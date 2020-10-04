c746dba1a154497f0a59555e01b76bc4b3b4fded
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-04 23:25:41 +0530

    01

--- lib/question.dart
+++ lib/question.dart
@@ -12,7 +12,7 @@ class Question extends StatelessWidget {
       margin: EdgeInsets.all(10),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        // color: LinearGradient(colors: Colors.accents),
+        // color: LinearGradient(colors: Colors.accents),10
       ),
       child: Text(
         questionText,
