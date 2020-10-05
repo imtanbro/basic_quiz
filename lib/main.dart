c1f0017c3b5060252b6d617f8e99e6c35733b1e7
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-05 22:57:58 +0530

    9

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class _MyAppState extends State<MyApp> {
         backgroundColor: Colors.black12,
         // body: Center(
         //   child: Text("My default Text",
-        //   style: TextStyle(2
+        //   style: TextStyle(
         //     color: Colors.white,
         //     fontSize: 25),),
         // ),
