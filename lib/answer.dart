2d045d7352789a438d23131b33da493518229363
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-06 14:05:00 +0530

    Custom Raise button

--- lib/answer.dart
+++ lib/answer.dart
@@ -5,6 +5,24 @@ class Answer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.amber,Colors.black])
+      ),
+      width: double.minPositive,
+      child: RaisedButton(
+                  child: Text(
+                    "Answer 2",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  onPressed: null,
+
+                  //if incase you want to pass a unique function to each button then.
+                  //onPressed: () => print("Hey"),
+                ),
       
     );
   }
